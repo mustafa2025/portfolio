1c1875cdfc58f699fa80c24caf2363cd1bf49145
Mustafa Ahmed <m.awes780@gmail.com>
2023-01-22 11:52:03 -0600

    changes

--- components/section/About.tsx
+++ components/section/About.tsx
@@ -1,7 +1,7 @@
 // react
-import * as React from 'react';
+import * as React from "react";
 // next
-import Image from 'next/image';
+import Image from "next/image";
 // @mui
 import {
   Box,
@@ -18,61 +18,61 @@ import {
   Collapse,
   useMediaQuery,
   useTheme,
-} from '@mui/material';
+} from "@mui/material";
 // custom component
-import FacebookIconLink from 'components/common/FacebookIconLink';
-import InstagramIconLink from 'components/common/InstagramIconLink';
-import TwitterIconLink from 'components/common/TwitterIconLink';
-import ContainerGrid from 'components/common/ContainerGrid';
-import ExpandMoreIconButton from 'components/common/ExpandMoreIconButton';
+import FacebookIconLink from "components/common/FacebookIconLink";
+import InstagramIconLink from "components/common/InstagramIconLink";
+import TwitterIconLink from "components/common/TwitterIconLink";
+import ContainerGrid from "components/common/ContainerGrid";
+import ExpandMoreIconButton from "components/common/ExpandMoreIconButton";
 // type
 interface AboutProps {}
 
 const ImageWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  position: 'relative',
-  width: '100%',
-  height: '21rem',
+  position: "relative",
+  width: "100%",
+  height: "21rem",
   [theme.breakpoints.up(350)]: {
-    height: '26rem',
+    height: "26rem",
   },
-  [theme.breakpoints.up('sm')]: {
-    height: '50vh',
+  [theme.breakpoints.up("sm")]: {
+    height: "50vh",
   },
-  [theme.breakpoints.up('md')]: {
-    height: '100%',
+  [theme.breakpoints.up("md")]: {
+    height: "100%",
   },
 }));
 
 const CustomContainer = styled(Box)<BoxProps>(({ theme }) => ({
-  scrollMarginTop: '1rem',
-  [theme.breakpoints.up('sm')]: {
-    scrollMarginTop: '2rem',
+  scrollMarginTop: "1rem",
+  [theme.breakpoints.up("sm")]: {
+    scrollMarginTop: "2rem",
   },
 }));
 
 const CustomCard = styled(Card)<CardProps>(({ theme }) => ({
-  margin: '1rem',
-  overflow: 'hidden',
-  [theme.breakpoints.up('sm')]: {
-    margin: '2rem',
+  margin: "1rem",
+  overflow: "hidden",
+  [theme.breakpoints.up("sm")]: {
+    margin: "2rem",
   },
-  [theme.breakpoints.up('md')]: {
-    height: 'fit-content',
+  [theme.breakpoints.up("md")]: {
+    height: "fit-content",
   },
-  [theme.breakpoints.up('lg')]: {
-    height: '90vh',
+  [theme.breakpoints.up("lg")]: {
+    height: "90vh",
   },
 }));
 
 const CustomCardActions = styled(CardActions)<CardActionsProps>(
   ({ theme }) => ({
-    justifyContent: 'space-between',
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      justifyContent: 'flex-end',
+    justifyContent: "space-between",
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      justifyContent: "flex-end",
     },
-    [theme.breakpoints.up('lg')]: {
-      position: 'absolute',
+    [theme.breakpoints.up("lg")]: {
+      position: "absolute",
       bottom: 0,
     },
   })
@@ -83,7 +83,7 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
   const [showMore, setShowMore] = React.useState(false);
 
   const theme = useTheme();
-  const isUpMd = useMediaQuery(theme.breakpoints.up('md'));
+  const isUpMd = useMediaQuery(theme.breakpoints.up("md"));
 
   React.useEffect(() => {
     if (isUpMd) setShowMore(true);
@@ -93,7 +93,7 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
   return (
     <CustomContainer id="about">
       <CustomCard>
-        <ContainerGrid sx={{ marginTop: 0, height: '100%' }}>
+        <ContainerGrid sx={{ marginTop: 0, height: "100%" }}>
           <Grid item xs={12} md={6} style={{ padding: 0 }}>
             <ImageWrapper>
               <Image
@@ -108,10 +108,10 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
                 <Skeleton
                   variant="rectangular"
                   sx={{
-                    backgroundColor: 'currentcolor',
-                    height: '100%',
-                    position: 'absolute',
-                    width: '100%',
+                    backgroundColor: "currentcolor",
+                    height: "100%",
+                    position: "absolute",
+                    width: "100%",
                   }}
                 />
               )}
@@ -121,26 +121,37 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
             item
             xs={12}
             md={6}
-            style={{ padding: '2rem', position: 'relative' }}
+            style={{ padding: "2rem", position: "relative" }}
           >
             <Card
-              sx={{ boxShadow: 'none', height: '100%', position: 'relative' }}
+              sx={{ boxShadow: "none", height: "100%", position: "relative" }}
             >
               <Typography component="h2" variant="h5" fontWeight="bold">
                 A bit about me
               </Typography>
               <CardContent>
                 <Typography component="p" variant="body1">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Dolorum exercitationem voluptate sapiente necessitatibus alias
-                  rerum voluptates accusamus tenetur, eaque natus possimus aut
-                  magni odio saepe eligendi laudantium. Enim, illo quod.
+                  {`Hi there! My name is Mustafa and I am a fullstack developer
+                  with a passion for building web applications that solve
+                  real-world problems. I have been working in the tech industry
+                  for 3 years. Throughout my career, I have gained experience in
+                  a variety of technologies, including Next.js and MongoDB. In
+                  my current role as a fullstack developer, I am responsible for
+                  designing and building web applications from start to finish,
+                  including everything from the front-end user interface to the
+                  back-end server-side logic. I am skilled in both front-end
+                  technologies like HTML, CSS, and JavaScript, as well as
+                  back-end technologies like Node.js, Express, and MongoDB. In
+                  addition to my technical skills, I am a strong communicator
+                  and thrive in collaborative environments. I am always eager to
+                  learn and am constantly seeking out new challenges to improve
+                  my skills and grow as a developer. `}
                 </Typography>
                 <Collapse
                   in={showMore}
                   sx={{
                     p: {
-                      marginTop: '0.5rem',
+                      marginTop: "0.5rem",
                     },
                   }}
                   timeout="auto"
@@ -164,7 +175,7 @@ const About: React.FunctionComponent<AboutProps> = (props) => {
                     open={showMore}
                     onClick={() => setShowMore(!showMore)}
                     sx={{
-                      justifySelf: 'flex-start',
+                      justifySelf: "flex-start",
                     }}
                   />
                 )}
